9af19f886cf5edccf7abce1cb9c9189db87070d0
daberg <daberg@openmailbox.org>
2019-03-22 20:10:07 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# dislog
+
+Python module implementing some discrete logarithm algorithms for study / research purposes.
+
+Licensed under the MIT License.
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    author='daberg',
+    description="Implementation of discrete logarithm algorithms",
+    name='dislog',
+    packages=['dislog'],
+    version='0.1'
+)
