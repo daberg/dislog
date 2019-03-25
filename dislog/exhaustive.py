8e84794746e909e7f4d5ccd3f5952b215c0ba98c
daberg <daberg@openmailbox.org>
2019-03-25 17:44:20 +0100

    Modify exhaustive docstring and add test

--- dislog/exhaustive.py
+++ dislog/exhaustive.py
@@ -2,21 +2,23 @@ from dislog.debug import debug
 
 
 def exhaustive(alfa, beta, n):
-    """Computes the discrete logarithm log_{alfa}(beta)
+    """Computes the discrete logarithm log_{alfa}(beta).
 
     Given a generator alfa of a cyclic group G, another element beta of the
-    group G and the order n of the the group G, computes the discrete logarithm
+    group G and the order n of the group G, computes the discrete logarithm
     of beta to the base of alpha.
 
     Args:
-        alfa: logarithm base, must support equality, multiplication and
-            exponentiation; should be a generator to guarantee the existence
-            of the logarithm
-        beta: logarithm argument, must support equality and multiplication
+        alfa: logarithm base, must support internal equality and multiplication
+            and integer exponentiation; should be a generator to guarantee the
+            existence of the logarithm
+        beta: logarithm argument, must support internal equality and
+            multiplication
         n: order of the group containing alfa and beta
 
     Returns:
-        The discrete logarithm log_{alfa}(beta) if it exists, None otherwise
+        The discrete logarithm log_{alfa}(beta) (the integer x such that alfa
+        to the power of x equals beta) if it exists, None otherwise
     """
     # Current exponent and power value being evaluated
     exp = 0
@@ -28,7 +30,7 @@ def exhaustive(alfa, beta, n):
     while power != beta:
         exp = exp + 1
 
-        # alfa is not a generator for Z*_{p}
+        # alfa is not a generator
         if exp == n:
             return None
 
---
+++ tests/test_exhaustive.py
@@ -0,0 +1,19 @@
+import dislog
+import unittest
+
+
+class ExhaustiveTestCase(unittest.TestCase):
+    def test_modulo_integer(self):
+        alfa = dislog.ModuloInteger(5, 97)
+        beta = dislog.ModuloInteger(35, 97)
+        n = 96
+
+        self.assertEqual(
+            dislog.exhaustive(alfa, beta, n),
+            32,
+            "Incorrect return value"
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
