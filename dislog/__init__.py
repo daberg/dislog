d1171e046ccd50e790380422bde5e29550f0b16f
daberg <daberg@openmailbox.org>
2019-03-25 17:45:14 +0100

    Fix typo

--- dislog/__init__.py
+++ dislog/__init__.py
@@ -6,7 +6,7 @@ Usage example:
     import dislog
 
 
-    # Initaliaze alfa (5) and beta (35) belonging to the multiplicative group
+    # Initialize alfa (5) and beta (35) belonging to the multiplicative group
     # of integers Z_{97}, of order 96, for which alfa is a generator
     alfa = dislog.ModuloInteger(5, 97)
     beta = dislog.ModuloInteger(35, 97)
