c5f17cfdbc8849be5776e655bb3bb71eaf2b74ea
daberg <daberg@openmailbox.org>
2019-03-27 18:18:13 +0100

    Add inverse method with test

--- dislog/modulointeger.py
+++ dislog/modulointeger.py
@@ -1,3 +1,4 @@
+from dislog.debug import debug
 from numbers import Number
 
 
@@ -50,3 +51,66 @@ class ModuloInteger:
 
     def __str__(self):
         return "{} (mod {})".format(self.value, self.modulus)
+
+    def inverse(self):
+        """Computes the ModuloInteger inverse of the instance.
+
+        With a and n respectively value and modulus of the ModuloInteger
+        instance, finds the value x s.t. ax = 1 mod n.
+
+        It does that by solving the equivalent diophantine equation
+        ax + ny = 1, using an adapted version of the extended euclidean
+        algorithm.
+
+        It can be proved that a solution exists iff gcd(a,n) = 1, that is iff a
+        is coprime to n.
+
+        In particular, x is found as the left coefficient of Bézout's identity:
+        ax + ny = gcd(a,n).
+
+        Args:
+            self: the ModuloInteger instance of which the inverse has to be
+                computed
+
+        Returns:
+            A ModuloInteger representing the inverse if it exists, None
+            otherwise
+        """
+        # Initialized with n and a respectively
+        r = self.modulus
+        r_new = self.value
+
+        x = 0
+        x_new = 1
+
+        debug("[ModuloInteger] Inverse of {}".format(self))
+        debug(
+            "[ModuloInteger]\t\tr={}\tr_new={}\tx={}\tx_new={}"
+            .format(r, r_new, x, x_new)
+        )
+
+        # Finds r = gcd(a,n) and left solution x
+        while r_new != 0:
+            quotient = r // r_new
+            r, r_new = r_new, r - quotient * r_new
+            x, x_new = x_new, x - quotient * x_new
+
+            debug(
+                "[ModuloInteger]\tq={}\tr={}\tr_new={}\tx={}\tx_new={}"
+                .format(quotient, r, r_new, x, x_new)
+            )
+
+        # No solution exists, since gcd(a, n) != 1 (a and n are not coprime)
+        if r > 1:
+            return None
+
+        # Left solutions are of the form: x + kn.
+        # For only two of them, abs(x) < n: these are called minimal
+        # solutions. One will be x* and the other -x*, with 0 < x* < n.
+        # Extended GCD finds either one or the other.
+        # If it finds the negative one, n must be added to make it positive.
+        if x < 0:
+            debug("[ModuloInteger] Adding {} to x".format(self.modulus))
+            x = x + self.modulus
+
+        return ModuloInteger(x, self.modulus)
---
+++ tests/test_modulointeger.py
@@ -0,0 +1,40 @@
+import dislog
+import unittest
+
+
+class ModuloIntegerTestCase(unittest.TestCase):
+    def test_modulointeger_inverse(self):
+        # Case list entry struture: (value, modulus, expected_inverse_value)
+        cases = [
+            (4, 9, 7),
+            (2, 3, 2),
+            (67, 119, 16),
+            (0, 3, None),
+            (1, 3, 1)
+        ]
+
+        for case in cases:
+            value = case[0]
+            modulus = case[1]
+            expected_inverse_value = case[2]
+
+            modulo_integer = dislog.ModuloInteger(value, modulus)
+            inverse = modulo_integer.inverse()
+            inverse_value = inverse.value if inverse else None
+
+            self.assertEqual(
+                inverse_value,
+                case[2],
+                "Incorrect inverse return value for {}".format(modulo_integer)
+            )
+
+            if expected_inverse_value:
+                self.assertEqual(
+                    (modulo_integer * inverse),
+                    dislog.ModuloInteger(1, modulus),
+                    "Multiplication result should be 1"
+                )
+
+
+if __name__ == '__main__':
+    unittest.main()
