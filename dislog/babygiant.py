7d89b481bc4b19096b5b719603c65aa43389c894
daberg <daberg@openmailbox.org>
2019-04-01 12:50:31 +0200

    Handle degenerate input in babygiant

--- dislog/babygiant.py
+++ dislog/babygiant.py
@@ -3,6 +3,24 @@ from dislog.debug import debug
 
 
 def babygiant(alfa, beta, n):
+    """Computes discrete logarithm using baby-step giant-step algorithm.
+
+    Given a generator alfa of a cyclic group G, another element beta of the
+    group G and the order n of the group G, computes the discrete logarithm
+    of beta to the base of alpha using the baby-step giant-step algorithm.
+
+    Args:
+        alfa: logarithm base, must support internal equality and
+            multiplication, integer exponentiation and hashing; should be a
+            generator to guarantee the existence of the logarithm
+        beta: logarithm argument, must support internal equality and
+            multiplication
+        n: order of the group containing alfa and beta
+
+    Returns:
+        The discrete logarithm log_{alfa}(beta) (the integer x such that alfa
+        to the power of x equals beta) if it exists, None otherwise
+    """
     m = math.ceil(math.sqrt(n))
 
     debug(babygiant, "alfa={}\tbeta={}\tn={}\tm={}", alfa, beta, n, m)
@@ -16,13 +34,16 @@ def babygiant(alfa, beta, n):
     exp_table[power] = 0
     for j in range(1, m):
         power = power * alfa
-        exp_table[power] = j
+        if power not in exp_table:
+            exp_table[power] = j
         debug(babygiant, "Adding {} : {} to table", power, j)
-        print(power, exp_table[power])
 
-    # At each step, candidate equals beta * (alfa ^ m)^i
-    factor = alfa ** -m
-    candidate = beta
+    factor = alfa ** (-m) # Candidate is multiplied by it at every iteration
+    if factor is None:
+        debug(babygiant, "alfa^-m does not exist, cannot use babygiant")
+        return None
+
+    candidate = beta # Equals beta * (alfa ^ m)^i at each iteration
     for i in range(m):
         debug(babygiant, "Checking presence of {} in lookup table", candidate)
         if candidate in exp_table:
