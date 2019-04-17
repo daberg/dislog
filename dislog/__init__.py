d067424303d6d28e854cb52ed3486344c2fb861a
daberg <daberg@openmailbox.org>
2019-04-17 20:01:12 +0200

    Add Pollard's algorithm

--- dislog/__init__.py
+++ dislog/__init__.py
@@ -12,14 +12,21 @@ Usage example:
     beta = dislog.ModuloInteger(35, 97)
     n = 96
 
-    # Compute the discrete logarithm log_{alpha}(beta) with an exhaustive search
+    # Compute the discrete logarithm of beta to the base alpha with an
+    # exhaustive search
     dislog.exhaustive(alpha, beta, n)
 """
 
-__all__ = ['babygiant', 'exhaustive', 'ModuloInteger', 'pohlighellman']
+__all__ = [
+    'babygiant', 'exhaustive', 'ModuloInteger', 'pohlighellman',
+    'expollard', 'modint_pollard_map', 'pollard'
+]
 
 
 from dislog.babygiant import babygiant
 from dislog.exhaustive import exhaustive
 from dislog.modulointeger import ModuloInteger
 from dislog.pohlighellman import pohlighellman
+from dislog.pollard import expollard
+from dislog.pollard import modint_pollard_map
+from dislog.pollard import pollard
---
+++ dislog/pollard.py
@@ -0,0 +1,120 @@
+from dislog import ModuloInteger
+from dislog.util import debug
+from sympy.core.numbers import mod_inverse
+from sympy.ntheory.primetest import isprime
+
+
+# _map helper list for alfa exponents
+_A_MAP = [
+    lambda a, n: a,
+    lambda a, n: (a * 2) % n,
+    lambda a, n: (a + 1) % n
+]
+
+# _map helper list for beta exponents
+_B_MAP = [
+    lambda b, n: (b + 1) % n,
+    lambda b, n: (b * 2) % n,
+    lambda b, n: b
+]
+
+# _map helper list for group elements
+_X_MAP = [
+    lambda x, alpha, beta: x * beta,
+    lambda x, alpha, beta: x ** 2,
+    lambda x, alpha, beta: x * alpha
+]
+
+# Maps a triple (alpha exponent, beta exponent, group element) to the next one,
+# according to the subset number of the group element
+def _map(a, b, x, alpha, beta, n, s_num):
+    debug(
+        pollard,
+        "Calculating _map for: s_num={} a={} b={} x={} n={}", s_num, a, b, x, n
+    )
+    a = _A_MAP[s_num](a, n)
+    b = _B_MAP[s_num](b, n)
+    x = _X_MAP[s_num](x, alpha, beta)
+    return (a, b, x)
+
+def pollard(alpha, beta, n, s_map, a_start=0, b_start=0):
+    debug(pollard, "alpha={} beta={} n={}", alpha, beta, n)
+    debug(pollard, "s_map={} a_start={} b_start={}", s_map, a_start, b_start)
+
+    # Initialization
+    a_slow = a_fast = a_start
+    b_slow = b_fast = b_start
+    x_slow = x_fast = (alpha ** a_start) * (beta ** b_start)
+
+    debug(pollard, "a={} b={} x={}", a_slow, b_slow, x_slow)
+    debug(pollard, "A={} B={} X={}", a_fast, b_fast, x_fast)
+
+    while True:
+        debug(pollard, "Increasing")
+
+        # Floyd's tortoise increase
+        a_slow, b_slow, x_slow = _map(
+            a_slow, b_slow, x_slow,
+            alpha, beta, n, s_map(x_slow)
+        )
+        debug(pollard, "a={} b={} x={}", a_slow, b_slow, x_slow)
+        assert((alpha ** a_slow) * (beta ** b_slow) == x_slow)
+
+        # Floyd's hare double increase
+        for _ in range(2):
+            a_fast, b_fast, x_fast = _map(
+                a_fast, b_fast, x_fast,
+                alpha, beta, n, s_map(x_fast)
+            )
+            debug(pollard, "A={} B={} X={}", a_fast, b_fast, x_fast)
+            assert((alpha ** a_fast) * (beta ** b_fast) == x_fast)
+
+        # Found loop
+        if x_slow == x_fast:
+            debug(pollard, "Found loop: x={} X={}", x_slow, x_fast)
+            debug(pollard, "            a={} A={}", a_slow, a_fast)
+            debug(pollard, "            b={} B={}", b_slow, b_fast)
+
+            assert(
+                (alpha ** a_slow) * (beta ** b_slow) ==
+                (alpha ** a_fast) * (beta ** b_fast)
+            )
+
+            r = (b_slow - b_fast) % n
+
+            debug(pollard, "(b - B) mod n = {}", r)
+
+            if r == 0:
+                return None
+
+            try:
+                r_inv = mod_inverse(r, n)
+
+            except ValueError:
+                debug(pollard, "Failure: r={} not invertible", r)
+                return None
+
+            debug(pollard, "r_inv={}", r_inv)
+
+            log = (r_inv * (a_fast - a_slow)) % n
+            debug(pollard, "Returning logarithm={}", log)
+
+            return log
+
+def expollard(alpha, beta, n, s_map):
+    debug(expollard, "alpha={} beta={} n={} s_map={}", alpha, beta, n, s_map)
+    for a_start in range(n):
+        for b_start in range(n):
+            ret = pollard(alpha, beta, n, s_map, a_start, b_start)
+            if ret is not None:
+                return ret
+    debug(expollard, "No suitable starting values were found")
+    return None
+
+
+# modint_pollard_map helper list
+_MODINT_MAP = [1, 0, 2]
+
+# Subset map for ModuloInteger instances
+def modint_pollard_map(modulo_integer_x):
+    return _MODINT_MAP[modulo_integer_x.value % 3]
