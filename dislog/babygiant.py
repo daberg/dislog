5bbb7265e0375ea9a169218871af58563a4b1a37
daberg <daberg@openmailbox.org>
2019-04-19 22:54:09 +0200

    Update babygiant function

--- dislog/babygiant.py
+++ dislog/babygiant.py
@@ -2,8 +2,24 @@ import decimal
 import math
 from dislog.util import debug
 
+def _round_sqrt(n):
+    # Conversion to handle decimal operations on big numbers
+    dec_n = decimal.Decimal(n)
 
-def babygiant(alpha, beta, n):
+    # Set precision for calculating the square root of n up to at least one
+    # digit beyond the decimal point
+    num_digits = dec_n.adjusted() + 1
+    prec = num_digits // 2 + 2
+    context = decimal.Context(prec=prec, rounding=decimal.ROUND_UP)
+
+    # Find root of order and round it up
+    dec_sqrt = dec_n.sqrt(context)
+    sqrt = int(dec_sqrt.to_integral_value(context=context))
+    debug(_round_sqrt, "sqrt={} rounded_sqrt={}", dec_sqrt, sqrt)
+
+    return sqrt
+
+def babygiant(alpha, beta, n, m=None):
     """Computes discrete logarithm using baby-step giant-step algorithm.
 
     Given a generator alpha of a cyclic group G, another element beta of the
@@ -17,6 +33,8 @@ def babygiant(alpha, beta, n):
         beta: logarithm argument, must support internal equality and
             multiplication
         n: order of the group containing alpha and beta
+        m: number of elements that the algorithm can store in memory; should be
+            less than n; if not specified, it is set to ceil(sqrt(n))
 
     Returns:
         The discrete logarithm log_{alpha}(beta) (the integer x such that alpha
@@ -24,19 +42,8 @@ def babygiant(alpha, beta, n):
     """
     debug(babygiant, "alpha={}\tbeta={}\tn={}\t", alpha, beta, n)
 
-    # Conversion to handle decimal operations on big numbers
-    dec_n = decimal.Decimal(n)
-
-    # Set precision for calculating the square root of n up to at least one
-    # digit beyond the decimal point
-    num_digits = dec_n.adjusted() + 1
-    prec = num_digits // 2 + 2
-    context = decimal.Context(prec=prec, rounding=decimal.ROUND_UP)
-
-    # Find root of order and round it up
-    dec_m = dec_n.sqrt(context)
-    m = int(dec_m.to_integral_value(context=context))
-    debug(babygiant, "m={} rounded_m={}", dec_m, m)
+    if m is None:
+        m = _round_sqrt(n)
 
     # Exponent lookup table
     # Key: a^j, value: j
@@ -58,7 +65,9 @@ def babygiant(alpha, beta, n):
 
     # At each iteration, candidate = beta * ((alpha ^ -m) ^ i)
     candidate = beta
-    for i in range(m):
+
+    # Check all powers up to the ceil of n / m not included
+    for i in range((n + m - 1) // m):
         debug(babygiant, "Checking presence of {} in lookup table", candidate)
         if candidate in exp_table:
             exp = i * m + exp_table[candidate]
