b7a223818e5b80f37ab814d8c02e0b3d3768f1aa
daberg <daberg@openmailbox.org>
2019-04-19 22:54:43 +0200

    Update tests

--- tests/test_dislog.py
+++ tests/test_dislog.py
@@ -5,18 +5,18 @@ from sympy.ntheory import factorint
 
 class DislogTestCase(unittest.TestCase):
     def test_modulo_integer(self):
-        # Entry structure: (alpha, beta, modulus, expected value)
+        # Entry structure: (alpha, beta, modulus, n, expected value)
         cases = [
-            (2, 1, 3, 0),
-            (5, 35, 97, 32),
-            (897, 654, 1709, None)
+            (2, 1, 3, 2, 0),
+            (33, 47, 50, 20, 19),
+            (897, 654, 1709, 1708, None)
         ]
 
         for case in cases:
             alpha = dislog.ModuloInteger(case[0], case[2])
             beta = dislog.ModuloInteger(case[1], case[2])
-            n = case[2] - 1
-            expected_value = case[3]
+            n = case[3]
+            expected_value = case[4]
 
             self.assertEqual(
                 dislog.exhaustive(alpha, beta, n),
